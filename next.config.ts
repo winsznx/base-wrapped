a8bbc30a962733ba6fdb2ac12c5dfd42b287c315
winsznx <timjosh507@gmail.com>
2025-12-16 14:17:46 +0100

    fix: add imagedelivery.net to allowed image domains
    - Neynar profile pictures are hosted on imagedelivery.net
    - Next.js Image component requires domain whitelisting
    - Fixes profile picture loading errors

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'i.imgur.com',
+      },
+    ],
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
